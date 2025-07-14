3514f123ab78d769ae9097600635f71725d74f7e
r.rostami <r.rostami@art.net>
2025-07-14 14:48:22 +0330

    Initial commit: create Maven project and .gitignore

---
+++ src/main/java/com/rashin/App.java
@@ -0,0 +1,13 @@
+package com.rashin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
